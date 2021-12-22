ecf7cee6cbd598f91619ed3cee8b271638c6f70f
benjaminaarsen <benjaminaarsen@outlook.com>
2021-12-22 23:34:22 +0100

    typo fixed

--- src/components/BezorgenText.vue
+++ src/components/BezorgenText.vue
@@ -4,7 +4,7 @@
             <h2>Bezorgen</h2>
             <p>Heeft u even geen tijd om lekker eten op tafel te zetten?
                 Dan hebben wij de perfecte oplossing voor u: wij komen 
-                de de lekkerste burgers van Nederland bij u aan de deur brengen
+                de lekkerste burgers van Nederland bij u aan de deur brengen
                 op de door u gekozen tijd. Betalen kan veilig en snel met iDeal,
                 zodat u niet aan de deur hoeft te betalen. 
             </p>
