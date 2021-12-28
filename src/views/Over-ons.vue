17d792c334c70a68608358b0e8b685b84a576771
Benjamin Aarsen <benjaminaarsen@outlook.com>
2021-12-28 18:57:27 +0100

    added "burgir" video to "over-ons"

--- src/views/Over-ons.vue
+++ src/views/Over-ons.vue
@@ -1,23 +1,28 @@
 <template>
     <div class="over-ons">
-        <img src="@/assets/main-image.jpg" />
+        <h1>Over ons</h1>
         <div class="text">
-            <h1>Over ons</h1>
-            <p>
-                Wij zijn burgir house, het lekkerste burgerrestaurant van Nederland, in 2018 hebben we ons restaurant geopend
-                en sindsdien zijn we veel gegroeid. We hopen met uw hulp, ondanks de huidige moeilijke tijden, binnenkort meerdere
-                locaties te openen zodat over in Nederland van onze burgers genoten kan worden. De betreffende locaties zijn: 
-            </p>
-            <ul>
-                <li>Amersfoort</li>
-                <li>Rotterdam</li>
-                <li>Den Haag</li>
-            </ul>
-            <p>
-                Andere locaties zullen natuurlijk ook in rekening genomen worden, reageer op onze twitter poll met uw locatie.
-                Dan kunnen wij beter inschatten waar we het hardst nodig zijn!
-            </p>
-            <a href="https://twitter.com" target="_blank"><Icon icon="akar-icons:twitter-fill" />Bezoek onze twitter pagina</a>
+            <article>
+                <p>
+                    Wij zijn Burgir House, het lekkerste burgerrestaurant van Nederland, in 2018 hebben we ons restaurant geopend
+                    en sindsdien zijn we veel gegroeid. We hopen met uw hulp, ondanks de huidige moeilijke tijden, binnenkort meerdere
+                    locaties te openen zodat over in Nederland van onze burgers genoten kan worden. De betreffende locaties zijn: 
+                </p>
+                <ul>
+                    <li>Amersfoort</li>
+                    <li>Rotterdam</li>
+                    <li>Den Haag</li>
+                </ul>
+                <p>
+                    Andere locaties zullen natuurlijk ook in rekening genomen worden, reageer op onze twitter poll met uw locatie.
+                    Dan kunnen wij beter inschatten waar we het hardst nodig zijn!
+                </p>
+                <a href="https://twitter.com" target="_blank"><Icon icon="akar-icons:twitter-fill" />Bezoek onze twitter pagina</a>
+            </article>
+            <article>
+                <h3>U vraagt zich vast af: hoe komen we aan de naam Burgir House?. De volgende video verklaart alles:</h3>
+                <iframe width="535" height="300" src="https://www.youtube.com/embed/yjNcXPtNQdE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            </article>
         </div>
     </div>
 </template>
@@ -33,31 +38,45 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+    h1 {
+        color: var(--secondary-color);  
+    }
     .over-ons {
-        padding: 50px;
-        display: flex;
-        flex-direction: row;
-        justify-content: center;
-        align-items: center;
         background: var(--primary-color);
     }
-        img {
-            height: 250px;
+     .text {
+            color: var(--primary-color);
+            display: grid;
+            padding: 10px 50px;
+            grid-template-columns: 1fr 1fr;
+            grid-column-gap: 80px;
+            justify-content: center;
+            // align-items: center;
+
         }
-        .text {
-            color: var(--secondary-color);
-            text-align: left;
-            padding: 10px 100px;
+        article {
+            margin-bottom: 150px;
+            background-color: var(--secondary-color);
+            border-radius: 30px;
+            padding: 10px 40px;
+            justify-content: center;
+            align-items: center;
+            flex-direction: column;
+            display: flex;
         }
             li {
                 font-weight: 500;
             }
+            iframe {
+                width: 535px;
+            }
             a {
                 font-weight: 500;
                 text-decoration: none;
-                color: var(--primary-color);
+                width: 50%;
+                color: var(--secondary-color);
                 padding: 10px 15px;
-                background-color: var(--secondary-color);
+                background-color: var(--primary-color);
                 border-radius: 10px;
             }
                 svg {
@@ -67,20 +86,28 @@ export default {
                 }
             a:hover {
                 cursor: pointer;
-                background-color: var(--secondary-color-shade);
+                background-color: var(--primary-color-shade);
             }
 @media screen and (max-width: 1200px) {
-    .over-ons {
-        flex-direction: column;
+    .text {
+        padding: 10px 40px;
+        text-align: center;
+        grid-template-columns: 1fr;
+        margin: 0 100px;
     }
-        .text {
-            padding: 10px 40px;
-            text-align: center;
-        }
 }
 @media screen and (max-width: 600px) {
     .text {
+        margin: 0;
         padding: 10px 0;
     }
+        iframe {
+            width: 100%;
+        }
+        article {
+            width: 80%;
+        }
+
+
 }
 </style>
