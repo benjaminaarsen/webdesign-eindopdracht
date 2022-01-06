305aa73deda7e3e24dfe9408c93fb1c930f37907
Benjamin Aarsen <benjaminaarsen@outlook.com>
2022-01-06 13:15:45 +0100

    added "contact" functionality to sidemenu

--- src/components/SideMenu.vue
+++ src/components/SideMenu.vue
@@ -10,9 +10,9 @@
             <router-link to="/over-ons" class="button">
                 <p>Over Ons</p>
             </router-link>
-            <router-link to="/contact" class="button">
+            <a href="#footer" class="button">
                 <p>Contact</p>
-            </router-link>
+            </a>
             <div class="close-button"><p>Menu sluiten</p><Icon icon="ep:circle-close-filled" /></div>
         </div>
     </div>
